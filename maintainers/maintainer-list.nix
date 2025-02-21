9e4e29949bda5ef0247b2d30ca4d665a6f2bfaab
CodeRadu <47945947+CodeRadu@users.noreply.github.com>
2025-02-21 17:50:43 +0100

    maintainers: add CodeRadu

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4532,6 +4532,11 @@
     email = "jupiter@m.rdis.dev";
     name = "Scott Little";
   };
+  CodeRadu = {
+    github = "CodeRadu";
+    githubId = 47945947;
+    name = "Radu";
+  };
   codgician = {
     email = "codgician@outlook.com";
     github = "codgician";
